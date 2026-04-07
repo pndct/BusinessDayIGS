a14c552ab0658fd4e12507107c92aecf97786cb7
m x <dskmiko@gmail.com>
2026-04-07 03:59:09 +0000

    Ubah length data kelas

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ const GENERAL_ADMIN = {
 };
 
 // --- DATA INITIAL SEEDING ---
-const INITIAL_CLASSES_DATA = Array.from({ length: 12 }, (_, i) => ({
+const INITIAL_CLASSES_DATA = Array.from({ length: 15 }, (_, i) => ({
   id: `class_${i + 1}`,
   name: `Kelas ${i + 1}`,
   order: i + 1,
